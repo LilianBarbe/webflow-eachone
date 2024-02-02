63cd8856f27e07cba4cc762a49aefa69d3769d57
LilianBarbe <110779804+LilianBarbe@users.noreply.github.com>
2024-02-02 16:44:03 +0100

    Possibilité de chercher par ville et code postal

--- src/form-test/region.js
+++ src/form-test/region.js
@@ -2,50 +2,45 @@ import "node_modules/@tarekraafat/autocomplete.js/dist/css/autoComplete.02.css";
 
 import autoComplete from "@tarekraafat/autocomplete.js";
 
-const select = document.querySelector("select[number]");
+// const select = document.querySelector("select[number]");
+// const setPays = async function () {
+//   const response = await fetch("https://restcountries.com/v3.1/all");
+//   const data = await response.json();
+//   await data.forEach((pays) => {
+//     const e = document.createElement("option");
+//     e.value = `name : ${pays.name.common}`;
+//     e.text = `${pays.name.common}`;
+//     // select.appendChild(e);
+//   });
+// };
 
-const setPays = async function () {
-  const response = await fetch("https://restcountries.com/v3.1/all");
-  const data = await response.json();
-  await data.forEach((pays) => {
-    const e = document.createElement("option");
-    e.value = `name : ${pays.name.common}`;
-    e.text = `${pays.name.common}`;
-    select.appendChild(e);
-  });
-};
-
-setPays();
+// setPays();
 
 const autoCompleteJS = new autoComplete({
   placeHolder: "Chercher une ville",
   data: {
     src: async (query) => {
       try {
-        const source = await fetch(
-          `https://geo.api.gouv.fr/communes?nom=${query}&boost=population`
-        );
+        const source = await fetch(`https://vicopo.selfbuild.fr/cherche/${query}`);
         const data = await source.json();
-        return data;
+        // Flatten the cities array to be directly usable by the autocomplete.
+        return data.cities;
       } catch (error) {
         return error;
       }
     },
-    // Data source 'Object' key to be searched
-    keys: ["nom"],
+    keys: ["city", "code"],
   },
   resultItem: {
     highlight: true,
     element: (item, data) => {
-      // Modify Results Item Style
       item.style = "display: flex; justify-content: space-between;";
-      // Modify Results Item Content
       item.innerHTML = `
       <span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
-        ${data.value.nom}
+        ${data.value.city}
       </span>
       <span style="display: flex; align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.3);">
-        ${data.value.codesPostaux[0]}
+        ${data.value.code}
       </span>`;
     },
   },
@@ -53,7 +48,7 @@ const autoCompleteJS = new autoComplete({
     input: {
       selection: (event) => {
         const selection = event.detail.selection.value;
-        autoCompleteJS.input.value = `${selection.nom} (${selection.codesPostaux[0]})`;
+        autoCompleteJS.input.value = `${selection.city} (${selection.code})`;
       },
     },
   },
