a69308aea654b2527056b3fe27e831607ddfa343
LilianBarbe <110779804+LilianBarbe@users.noreply.github.com>
2024-04-05 09:51:24 +0200

    correct bugs

--- dist/form-test/swiper-logo.js
+++ dist/form-test/swiper-logo.js
@@ -1,6 +1,1 @@
-"use strict";
-(() => {
-  // bin/live-reload.js
-  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
-})();
-//# sourceMappingURL=swiper-logo.js.map
+"use strict";(()=>{})();
