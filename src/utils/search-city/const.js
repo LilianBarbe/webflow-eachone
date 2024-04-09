d40679ed9866482bfb9e493ead6ebbe4caf143fa
LilianBarbe <110779804+LilianBarbe@users.noreply.github.com>
2024-04-09 16:26:26 +0200

    All forms with address

--- src/utils/search-city/const.js
+++ src/utils/search-city/const.js
@@ -1,9 +1,9 @@
-export const inputHabitationFull = document.querySelector("[data-address='full']");
-export const addressList = document.querySelector("[data-address-list]");
-export const inputVille = document.querySelector("[data-empty-ville]");
-export const inputCodePostal = document.querySelector("[data-empty-codepostal]");
-export const inputNumberStreet = document.querySelector("[data-empty-numberstreet]");
-export const textAddress = document.querySelector("[data-address-item]");
+export const inputHabitationFull = form.querySelector("[data-address='full']");
+export const addressList = form.querySelector("[data-address-list]");
+export const inputVille = form.querySelector("[data-empty-ville]");
+export const inputCodePostal = form.querySelector("[data-empty-codepostal]");
+export const inputNumberStreet = form.querySelector("[data-empty-numberstreet]");
+export const textAddress = form.querySelector("[data-address-item]");
 export const parent = textAddress.parentNode;
 export const addressText = "[data-adress-label]";
 export const addressPostalText = "[data-adress-postal]";
